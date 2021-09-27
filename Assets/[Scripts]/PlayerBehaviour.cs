d1539e13722a464247b4a75f60d810246e24c6ee
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-27 12:25:43 -0400

    Added simple Player  Animation

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -7,17 +7,24 @@ public class PlayerBehaviour : MonoBehaviour
     public float horizontalForce;
     public float verticalForce;
     public bool isGrounded;
-
-    private Rigidbody2D rigidBody2D;
     
+    private Animator animatorController;
+    private Rigidbody2D rigidBody2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animatorController = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         float x = 0.0f;
         float y = 0.0f;
@@ -30,9 +37,23 @@ public class PlayerBehaviour : MonoBehaviour
             // check if player is moving
             if (x != 0)
             {
-                transform.localScale = new Vector3(Mathf.RoundToInt(x), 1.0f);
+                // uses the Ternary operator
+                x = (x > 0) ? 1 : -1;
+
+                transform.localScale = new Vector3(x, 1.0f);
+
+                // shift to run animation
+                animatorController.SetInteger("AnimationState", 1); // run
             }
-            
+            else
+            {
+                // shift back to idle
+                animatorController.SetInteger("AnimationState", 0); // idle
+            }
+        }
+        else
+        {
+            animatorController.SetInteger("AnimationState", 2); // jump
         }
 
         Vector2 movementVector = new Vector2(x * horizontalForce, y * verticalForce);
