5b549062b40963f5f161ff68d6407a088226aa4b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-27 11:02:10 -0400

    Added Project Files

---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float horizontalForce;
+    public float verticalForce;
+    public bool isGrounded;
+
+    private Rigidbody2D rigidBody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+
+        if (isGrounded)
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            y = Input.GetAxisRaw("Jump");
+        }
+
+        Vector2 movementVector = new Vector2(x * horizontalForce, y * verticalForce);
+        rigidBody2D.AddForce(movementVector);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = false;
+        }
+    }
+}
